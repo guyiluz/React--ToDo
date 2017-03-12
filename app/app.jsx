2790a682dbc20e0f4398516c3bc2e8a053fbe1ad
Your Name <you@example.com>
2017-03-12 17:14:44 +0200

    v1

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
+var Todoapp=require('Todoapp')
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -11,6 +11,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-<p>Bol3</p>,
+<Todoapp />,
   document.getElementById('app')
 );
